1c833a8348ee4e82a33f736164c3ca26c1a80418
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-09-11 00:41:01 +0530

    Update simple_interest_calculator.py

--- simple_interest_calculator.py
+++ simple_interest_calculator.py
@@ -3,21 +3,26 @@
 # User input
 principal = 0
 rate = 0
-time = int(input("Enter the time period (in years): "))
+time = 0
 
 # Calculation
 
 while principal <= 0:
   principal = float(input("Enter the principal amount (₹): "))
-  if principle <=0:
+  if principal <=0:
     print("principal can't be less than or equal to zero.")
 
 while rate <= 0:
   rate = float(input("Enter the annual interest rate (%): "))
   if rate <=0:
-    print("rate can't be less than or equal to zero.")
+    print("interest rate can't be less than or equal to zero.")
+    
+while time <= 0:
+  time = int(input("Enter the time (in years): "))
+  if rate <=0:
+    print("time can't be less than or equal to zero.")
 interest = (principal * rate * time) / 100
 
 # Output
-print(f"\nFor ₹{principal} at {rate}% for {time} years:")
+print(f"\nFor principal amount of ₹{principal} at interest rate {rate}% for {time} years:")
 print(f"Simple Interest = ₹{interest:.2f}")
