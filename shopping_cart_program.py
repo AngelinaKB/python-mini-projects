67e0df814d94d74e93bde0451e5da7ba296f08ae
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-18 21:18:30 +0530

    Update shopping_cart_program.py

--- shopping_cart_program.py
+++ shopping_cart_program.py
@@ -1,9 +1,24 @@
-item = input("What do you wish to buy?: ")
-price= float(input("Enter the price of the item: "))
-quantity= int(input("How many would you like?: "))
-total = price*quantity
+foods = []
+prices = []
+total = 0
 
-print("\nShoping Cart")
-print(f"Item: {item}/s")
-print(f"Quantity: {quantity}")
-print(f"Your total is: ${total:.2f}")
+while True:
+  food = input("Enter a food to buy (press q to quit): ")
+  if food.lower() == "q":
+    break
+  else:
+    price =  float(input(f"Enter the price of the {foods}: $"))
+    foods.append(food.capitalize())
+    prices.append(price)
+
+print("----- Your Cart -----")
+for food in foods:
+  print(food, end=" ")
+  
+for price in prices:
+  total += price
+
+print()
+print(f"Your total is: ${total}")
+    
+
