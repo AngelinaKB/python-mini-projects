b3e5ee54ae0a846916ff1ea65dec60947f755b10
Angelina Bawa <167961513+AngelinaKB@users.noreply.github.com>
2025-10-23 21:41:11 +0530

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -5,7 +5,7 @@ import random
 # ● ┌ ─ ┐ │ └ ┘
 # print("\u25CF \u250C \u2500 \u2510 \u2502 \u2514 \u2518")
 
-DIE_ART = {
+dice_art = {
     1: (
         "┌───────┐",
         "│       │",
@@ -50,10 +50,20 @@ DIE_ART = {
     ),
 }
 
-dice = []
+dice = [] # list of dice
 total = 0
 num_of_dice = int(input("How many dice?: "))
 
-for die in  
-dice.append(random.randint(1,6))
+for die in range(num_of_dice):
+    dice.append(random.randint(1,6))
 
+for line in range(5):
+    for die in dice:
+        print(dice_art.get(die)[line], end="")
+    print()
+        
+for die in dice:
+    total += die
+    
+
+print(f"total: {total}")
